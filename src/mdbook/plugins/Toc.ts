6121751fe9422f37f0aa715c5882dccfe271a368
Jontze <42588836+jontze@users.noreply.github.com>
2021-09-14 17:27:33 +0200

    feat: Add support of mdbook-toc plugin

---
+++ src/mdbook/plugins/Toc.ts
@@ -0,0 +1,12 @@
+import { MdPlugin } from "./MdPlugin";
+
+export class Toc extends MdPlugin {
+  constructor() {
+    super(
+      "badboy/mdbook-toc",
+      "toc-version",
+      "mdbook-toc",
+      "unknown-linux-gnu"
+    );
+  }
+}
